664d3a405ce2f5539cfc0e45b0a7864227d0faf7
faranbutt <faranbutt789@gmail.com>
2023-08-11 19:49:49 +0500

    Pushing React Table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
    <div  className='text-7xl font-bold flex justify-center items-center text-transparent bg-clip-text bg-gradient-to-l from-red-500 to bg-orange-300'>
      React Table with Manitine UI
    </div>
-   <Comp  data={data} />
+   <Comp/>
    </main>
   
     </>
